7e614c6380f6437a8b16d1f73443c229f21689b7
Tyler <tyler.thms@gmail.com>
2026-08-24 14:42:30 +0200

    chore: add nativewind config files and update layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,6 @@ import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useColorScheme } from "react-native";
 
-import { AnimatedSplashOverlay } from "@/components/animated-icon";
-
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -11,7 +9,6 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
       <Stack
         screenOptions={{
           headerStyle: {
