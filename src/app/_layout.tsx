2776fd609d7cfbe44eb492717e145152c978d570
Tyler Thomas <tyler@optonix.co.za>
2026-08-25 07:19:46 +0200

    fix(SHOP-6): remove logic to prevent the hiding of splash screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,6 @@
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
 import { useColorScheme } from "react-native";
 import "../global.css";
-
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
